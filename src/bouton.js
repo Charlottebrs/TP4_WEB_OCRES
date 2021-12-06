d6c7f8f3d61f5b80d5bfd90cbbb26848cbdfaccb
charlotte <brasseurcharlotte28@gmail.com>
2021-12-06 17:02:45 +0100

    ok

---
+++ src/bouton.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+export default class Bouton extends React.Component{
+    render(){
+        return(
+            <div>{this.props.text}</div>
+        )
+    }
+}
+
+
